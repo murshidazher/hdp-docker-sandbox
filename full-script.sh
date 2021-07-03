40e7c38ebafbe54df3f712b91198e834871bdb04
murshidazher <murshidazher@yahoo.com>
2021-07-04 05:07:00 +0530

    feat: add volume mapping

---
+++ full-script.sh
@@ -0,0 +1,21 @@
+# https://gist.github.com/npearce/6f3c7826c7499587f00957fee62f8ee9
+
+# It additionally installs git/yum-cron, 
+# configures yum-cron to automatically install security updates hourly, 
+# and sets a system wide env to specify that I want the alpine tag of docker-compose.
+
+sudo yum -y install git docker yum-cron; \
+ sudo sed -i '/update_cmd/s/= .*/= security/' /etc/yum/yum-cron-hourly.conf; \
+ sudo sed -i '/update_messages/s/= .*/= no/' /etc/yum/yum-cron-hourly.conf; \
+ sudo sed -i '/apply_updates/s/= .*/= yes/' /etc/yum/yum-cron-hourly.conf; \
+ sudo sed -i '/download_updates/s/= .*/= yes/' /etc/yum/yum-cron-hourly.conf; \
+ sudo sed -i '/download_updates/s/= .*/= no/' /etc/yum/yum-cron.conf; \
+ sudo service yum-cron start; \
+ sudo chkconfig yum-cron on; \
+ sudo service docker start; \
+ sudo usermod -a -G docker ec2-user; \
+ sudo chkconfig docker on; \
+ sudo sh -c "echo 'export DOCKER_COMPOSE_IMAGE_TAG=ghcr.io/linuxserver/docker-compose:alpine' >> /etc/profile.d/dockerComposeAlpineImageTagEnv.sh"; \
+ sudo curl -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose; \
+ sudo chmod +x /usr/local/bin/docker-compose; \
+ sudo reboot;
--- mount_voulme.sh
+++ mount_voulme.sh
@@ -2,10 +2,24 @@
 
 set -eux -o
 
+# auto start docker - put on the start up file of hdp
+sudo chkconfig docker on
+
+# install docker compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-centos-7
 # https://docs.docker.com/storage/volumes/
 
-docker-compose up -d
+docker-compose up -d && docker ps
 
 # after first time we can use start
-docker-compose start
+#docker-compose start
+
+# list services
+docker volume ls
+
+# docker volume inspect <volume_name>
 
