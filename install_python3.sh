b1a6670f0e40ee3b25ee712f85b92bb7af498248
murshidazher <murshidazher@yahoo.com>
2021-07-04 08:35:26 +0530

    feat: add more setup scripts

--- install_python3.sh
+++ install_python3.sh
@@ -1,3 +1,4 @@
+# typings error https://stackoverflow.com/questions/65869296/installing-pip-is-not-working-in-python-3-6/65871131#65871131
 yum install -y python34 python34-pip python34-devel gcc-c++
 python3.4 --version
 pip3.4 install --upgrade pip
