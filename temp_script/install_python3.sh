1b503c8a7172dd54048c6865235dcd3fde8133e1
murshidazher <murshidazher@yahoo.com>
2021-07-08 02:29:53 +0530

    :bug: fix: add pip3 library installation

--- temp_script/install_python3.sh
+++ temp_script/install_python3.sh
@@ -4,8 +4,8 @@ yum -y remove python36u python36u-devel python36u-pip python36u-setuptools pytho
 yum -y install python36 python36-devel python36-pip python36-setuptools python36-tools python36-libs python36-tkinter gcc-c++
 yum -y install cmake3 cmake3-data
 python3 --version
+pip3 wheel pandas numpy scipy scikit-learn matplotlib virtualenv
 pip3 install jupyter
 pip3 install kaggle
-
 
 kaggle datasets download hk7797/stock-market-india
