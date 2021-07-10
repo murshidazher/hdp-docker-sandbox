00f29354275cd6883940dd86e144a603fa28a25d
murshidazher <murshidazher@yahoo.com>
2021-07-11 02:13:45 +0530

    :sparkles: feat: update pyspark to pyspark2

--- temp_script/jupyter-spark-installation.sh
+++ temp_script/jupyter-spark-installation.sh
@@ -43,12 +43,12 @@ USER=$1
 JUPYTER_HOST=sandbox-hdp.hortonworks.com
 JUPYTER_PORT=8889
 su - ${USER} <<EOF
-export SPARK_HOME=/usr/hdp/current/spark-client
+export SPARK_HOME=/usr/hdp/current/spark2-client
 export PYSPARK_SUBMIT_ARGS="--master yarn-client pyspark-shell"
 export HADOOP_HOME=/usr/hdp/current/hadoop-client
 export HADOOP_CONF_DIR=/usr/hdp/current/hadoop-client/conf
-export PYTHONPATH="/usr/hdp/current/spark-client/python:/usr/hdp/current/spark-client/python/lib/py4j-0.9-src.zip"
-export PYTHONSTARTUP=/usr/hdp/current/spark-client/python/pyspark/shell.py
+export PYTHONPATH="/usr/hdp/current/spark2-client/python:/usr/hdp/current/spark2-client/python/lib/py4j-0.10.6-src.zip"
+export PYTHONSTARTUP=/usr/hdp/current/spark2-client/python/pyspark/shell.py
 export PYSPARK_SUBMIT_ARGS="--master yarn-client pyspark-shell"
 echo "Starting Jupyter daemon on HDP Cluster ..."
 jupyter notebook --config=/ibm/conf/jupyter_notebook_config.py --ip=${JUPYTER_HOST} --port=${JUPYTER_PORT}&
